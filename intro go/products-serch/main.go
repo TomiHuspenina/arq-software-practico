14c72ddc5f7f805adb14313ea9ce7ccc736c8661
TomiHuspenina <127957400+TomiHuspenina@users.noreply.github.com>
2024-04-18 15:39:17 -0300

    Add files via upload

--- intro go/products-serch/main.go
+++ intro go/products-serch/main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
 type Result struct {
 	Title             string  `json:"title"`
 	Price             float64 `json:"price"`
-	AvailableQuantity int     `json:"avaiable_quantity"`
+	AvailableQuantity int     `json:"available_quantity"`
 }
 
 type Response struct {
@@ -20,18 +21,25 @@ type Response struct {
 }
 
 func main() {
-
-	fmt.Println("Busca un producto: ")
+	name := "Productos"
+	CreateFile(name)
+	fmt.Print("Busca un Producto: ")
 	input := readInput()
 	fmt.Println(input)
 
-	results := search(input)
-	for i, result := range results {
-		fmt.Println(fmt.Sprintf(" [%d] %s", i, result.Title)) // en comillas dobles esta la variable donde voy a generar, %d es int, %s string
-		//[0] Celular Samsung S23
-		time.Sleep(200 * time.Millisecond)
+	file, err := os.OpenFile(name, os.O_APPEND|os.O_WRONLY, 0644) //os.O_APPEND indica que se debe agregar al final del archivo.
+	if err != nil {                                               //os.O_WRONLY indica que el archivo se abrirá en modo de solo escritura.
+		fmt.Println("Error abriendo archivo:", err) //argumento 0644 especifica los permisos del archivo.
+		return
 	}
 
+	results := search(input)
+	for i, result := range results {
+		line := fmt.Sprintf(" [%d] %s \n", i, result.Title)
+		fmt.Println(line)
+		file.WriteString(line)
+		time.Sleep(200 * time.Millisecond)
+	}
 }
 
 func readInput() string {
@@ -67,6 +75,15 @@ func search(query string) []Result {
 
 	return searchResponse.Results
 
+}
+
+func CreateFile(path string) error {
+	file, err := os.Create(path)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // 1. Input de busqueda
